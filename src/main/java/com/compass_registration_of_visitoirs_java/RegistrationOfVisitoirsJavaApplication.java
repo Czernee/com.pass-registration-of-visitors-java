6038af437efbec7c48d0c496497f10bacc5e5669
Daniil Chernousov <i@tehnik-7.ru>
2024-10-03 14:37:09 +0300

    init

---
+++ src/main/java/com/compass_registration_of_visitoirs_java/RegistrationOfVisitoirsJavaApplication.java
@@ -0,0 +1,13 @@
+package com.compass_registration_of_visitoirs_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationOfVisitoirsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationOfVisitoirsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compass_registration_of_visitoirs_java/RegistrationOfVisitoirsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass_registration_of_visitoirs_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationOfVisitoirsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
