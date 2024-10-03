f23caa1b86a776d12fe22acee84e0236ea161dd3
Daniil Chernousov <i@tehnik-7.ru>
2024-10-03 15:01:33 +0300

    model created

---
+++ src/main/java/com/compass_registration_of_visitoirs_java/models/Client.java
@@ -0,0 +1,74 @@
+package com.compass_registration_of_visitoirs_java.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public void setPassport(String passport) {
+        this.passport = passport;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public Date getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(Date arrival) {
+        this.arrival = arrival;
+    }
+
+    public Date getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(Date departure) {
+        this.departure = departure;
+    }
+
+    private String fullname, passport, phone, room;
+    private Date arrival, departure;
+}
---
+++ src/main/java/com/compass_registration_of_visitoirs_java/repo/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.compass_registration_of_visitoirs_java.repo;
+
+import com.compass_registration_of_visitoirs_java.models.Client;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClientRepository extends CrudRepository<Client, Long> {
+}
